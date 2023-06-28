2c1af085546c22e23574a490bdec43605b43a7d2
alone <samuraii.project143@gmail.com>
2023-06-28 12:52:01 +0300

    feart: add all corse

--- middlewares/__init__.py
+++ middlewares/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from .context import ContextMiddleware
 from .session import SessionMiddleware
 
@@ -7,5 +7,12 @@ from .session import SessionMiddleware
 def register_middlewares(app: FastAPI) -> FastAPI:
     app.add_middleware(ContextMiddleware)
     app.add_middleware(SessionMiddleware)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     return app
